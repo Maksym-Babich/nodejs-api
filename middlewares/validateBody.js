0176fa130a9b929a6a22f99e146278ba62fed01d
Maksym-Babich <maxus1042@gmail.com>
2023-06-02 23:44:24 +0300

    corrections added

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,23 +1,20 @@
+const { object } = require('joi');
 const { HttpError } = require('../utils');
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     let message = '';
     const { error } = schema.validate(req.body);
-
-    if (error) {
-      switch (req.method) {
-        case 'PUT':
-          message = 'missing fields';
-          break;
-        case 'POST':
-          message = `missing required ${error?.details[0]?.context.label} field`;
-          break;
-        default:
-          break;
-      }
+    console.log(Object.keys(req.body).length);
+    if (Object.keys(req.body).length === 0) {
+      message = 'missing fields';
       next(HttpError(400, message));
     }
+    if (error) {
+      message = `missing required ${error?.details[0]?.context.label} field`;
+      next(HttpError(400, message));
+    }
+
     next();
   };
   return func;
