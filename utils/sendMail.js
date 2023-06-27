11eced9d627ac28cecfd090b4edcdef613c38a71
Maksym-Babich <maxus1042@gmail.com>
2023-06-27 22:36:54 +0300

    day1

---
+++ utils/sendMail.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+
+const config = {
+  host: 'smtp.ukr.net',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'maxus195@ukr.net',
+    pass: process.env.IMAP_PASS,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const sendMail = async (data) => {
+  const email = { from: 'maxus195@ukr.net', ...data };
+  await transporter.sendMail(email);
+  return true;
+};
+
+module.exports = sendMail;
